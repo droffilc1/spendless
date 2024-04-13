431ceda352c3cd4e25f3cd50b6d2de993d6d407a
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-04-14 00:28:43 +0300

    chore: update links

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
-import { FaFacebook, FaInstagramSquare, FaLinkedin  } from "react-icons/fa";
-import { FaSquareXTwitter, FaTiktok } from "react-icons/fa6";
+import { FaGithub, FaLinkedin  } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 
 const Footer = () => {
@@ -13,20 +13,14 @@ const Footer = () => {
       </nav>
 
       <div className="flex justify-center space-x-5 text-2xl">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook />
-        </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/clifford-mapesa-a98601216/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagramSquare />
-        </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/droffilcasepam1" target="_blank" rel="noopener noreferrer">
           <FaSquareXTwitter />
         </a>
-        <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
-          <FaTiktok />
+        <a href="https://github.com/droffilc1/spendless" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
         </a>
       </div>
       <p className="text-center text-gray-700 font-medium">&copy; {new Date().getFullYear()} Company Ltd. All rights reserved.</p>
